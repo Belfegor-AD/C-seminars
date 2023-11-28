64bb50ec797568dd1c64d15dedf5964e2feedebf
Andrey Plotnikov <>
2023-11-28 22:53:13 +0700

    Completed 4 task

---
+++ Lesson02/Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine()!);
+while (number > 0)
+{
+    int remains = number % 10;
+    number /= 10;
+    if (number > 0)
+        Console.Write($"{remains}, ");
+    else
+        Console.Write(remains);
+}
