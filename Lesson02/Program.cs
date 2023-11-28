0c0ef54acd9aa05692e84ff89b756e21df9eebd1
Andrey Plotnikov <>
2023-11-28 10:10:08 +0700

    Сделал 1 задание 2 семинара

---
+++ Lesson02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine($"{number} кратно 7 и 23");
+else
+    Console.WriteLine($"{number} не делится без остатка на 7 и 23");
