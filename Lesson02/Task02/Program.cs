d687c8924994bce0ebce6914e3b1f4eb20f20520
Andrey Plotnikov <>
2023-11-28 21:47:39 +0700

    Completed 2 task

---
+++ Lesson02/Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Веедите координаты точки, не равные 0.");
+Console.Write("Координата X:");
+int coordinateX = int.Parse(Console.ReadLine()!);
+Console.Write("Координата Y:");
+int coordinateY = int.Parse(Console.ReadLine()!);
+if (coordinateX > 0 && coordinateY > 0)
+    Console.WriteLine("Точка в 1 четверти");
+else
+    if (coordinateX < 0 && coordinateY > 0)
+        Console.WriteLine("Точка в 2 четверти");
+    else
+        if (coordinateX < 0 && coordinateY < 0)
+            Console.WriteLine("Точка в 3 четверти");
+        else
+            if (coordinateX > 0 && coordinateY < 0)
+                Console.WriteLine("Точка в 4 четверти");
+            else
+                Console.WriteLine("Точка на пересечении линии координат");
