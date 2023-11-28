d689deb9fb84ded66b751b704e3cc966671709cc
Andrey Plotnikov <>
2023-11-28 22:14:06 +0700

    Completed 3 task

--- Lesson02/Task02/Program.cs
+++ Lesson02/Task02/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Clear();
 Console.WriteLine("Веедите координаты точки, не равные 0.");
-Console.Write("Координата X:");
+Console.Write("Координата X: ");
 int coordinateX = int.Parse(Console.ReadLine()!);
-Console.Write("Координата Y:");
+Console.Write("Координата Y: ");
 int coordinateY = int.Parse(Console.ReadLine()!);
 if (coordinateX > 0 && coordinateY > 0)
     Console.WriteLine("Точка в 1 четверти");
---
+++ Lesson02/Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите целое число от 10 до 99: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number >= 10 && number <= 99)
+    if (number / 10 > number % 10)
+        Console.WriteLine($"1я цифра больше 2й");
+    else
+        if (number / 10 < number % 10)
+            Console.WriteLine($"2я цифра больше 1й");
+        else
+            Console.WriteLine("Цифры равны");
+else
+    Console.WriteLine("Не верное число");
