d5d5d7b0b1b80d240b0ccd48e6367c2604f2687b
Andrey Plotnikov <>
2023-12-01 11:32:00 +0700

    Completed Lesson03  Task01

---
+++ Lesson03/Task01/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] fullArray)
+{
+    Console.Write($"Массив чисел: ");
+    foreach (int item in fullArray)
+    {
+        Console.Write(item + " ");
+    }
+}
+
+int FindInArray(int[] fullArray, int beginRange, int endRange)
+{
+    int count = 0;
+    foreach (int item in fullArray)
+    {
+        if (item >= beginRange && item <= endRange)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+int sizeArray = 10;
+int beginRange = 20, endRange = 90;
+int[] array = CreateArray(sizeArray);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Количество элементов в диапазоне [{beginRange},{endRange}]: " + FindInArray(array, beginRange, endRange));
