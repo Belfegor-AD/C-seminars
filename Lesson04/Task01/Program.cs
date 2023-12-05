b2f3dda77de11b8e4f432d20050fd3411a8d2fd8
Andrey Plotnikov <>
2023-12-05 16:14:20 +0700

    Complete Lesson04 Task01

---
+++ Lesson04/Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+Console.Clear();
+string number = " ";
+while (Exit(number))
+{
+    Console.WriteLine("Еще попытка.");
+}
+
+bool Exit(string num)
+{
+    Console.Write("Введите целое число или 'q': ");
+    num = Console.ReadLine()!;
+    if (num == "q")
+        return false;
+    int sum = 0;
+    for(int i = 0; i < num.Length; i++)
+    {
+        int m = int.Parse(num[i].ToString());
+        sum = sum + m;
+    }
+    Console.Write($"Сумма цифр: {sum}");
+    if (sum % 2 == 0)
+    {
+        Console.WriteLine(" чётная");
+        return false;
+    }
+    Console.WriteLine(" не чётная");
+    return true;
+}
+    
