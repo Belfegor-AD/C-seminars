5ebad7dba1e77b82f920db481db56b339d11eded
Andrey Plotnikov <>
2023-12-05 17:06:16 +0700

    Complete Lesson04 Task03

---
+++ Lesson04/Task03/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] CreateArray(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(begin, end);
+    }
+    return array;
+}
+
+void Flipping(int[] arr)
+{
+    for (int i = 0; i <= arr.Length/2; i++)
+    {
+        int j = arr.Length - 1 - i;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"Массив чисел: ");
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+int[] array = CreateArray(10, 0, 50); //Создание массива из 10 значений в диапазоне [0,50]
+PrintArray(array);
+Flipping(array);
+PrintArray(array);
