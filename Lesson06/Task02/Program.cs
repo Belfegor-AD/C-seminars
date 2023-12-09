a28e1c35c7ad8ca57672be748e578aede05be715
Andrey Plotnikov <>
2023-12-09 20:58:48 +0700

    Lesson06 Task02 is complited

---
+++ Lesson06/Task02/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах.
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+Console.Clear();
+string str = "Hello World";
+Console.WriteLine($"Изначальная строка: {str}");
+string strLower = str.ToLower();
+Console.WriteLine($"Итоговая строка: {strLower}");
