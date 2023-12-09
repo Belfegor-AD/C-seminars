5c272615d6cc7e2880b30b2334f26d5ac7b9a435
Andrey Plotnikov <>
2023-12-09 20:32:39 +0700

    Lesson06 Task01 is coplited

---
+++ Lesson06/Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+
+
+Console.Clear();
+char[,] chars = {{'H', 'e', 'l', 'l', 'o', ' '}, {'w', 'o', 'r', 'l', 'd', '!'}};
+string result = string.Empty;
+Console.Write($"Начальный массив: ");
+for (int i = 0; i < chars.GetLength(0); i++)
+{
+    for (int j = 0; j < chars.GetLength(1); j++)
+    {
+        Console.Write($"'{chars[i, j]}' ");
+        result += chars[i, j];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Итоговая строка: {result}");
