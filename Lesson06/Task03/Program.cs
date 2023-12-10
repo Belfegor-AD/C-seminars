a60cf9bdf39df5c1bfae708fbd7ab2cd77e87fe0
Andrey Plotnikov <>
2023-12-10 09:40:25 +0700

    Lesson06 Task03 is complited

---
+++ Lesson06/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string ReversStr(string strLower)
+{
+    string newStr = string.Concat(strLower.ToArray().Reverse().ToArray());
+    Console.WriteLine($"Перевёрнутое слово: {newStr}");
+    return newStr;
+}
+
+
+Console.Clear();
+string str = "Шалаш";
+Console.WriteLine($"Проверяемое слово: {str}");
+string strLower = str.ToLower();
+
+if (strLower == ReversStr(strLower))
+    Console.WriteLine($"Слово {str} является палиндромом");
+else
+    Console.WriteLine($"Слово {str} не является палиндромом");
