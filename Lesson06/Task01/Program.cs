9dccb94fc386dce42e3f73235b7c0ec50719f8b4
Andrey Plotnikov <>
2023-12-10 09:52:11 +0700

    Task01, 03 edits

--- Lesson06/Task01/Program.cs
+++ Lesson06/Task01/Program.cs
@@ -2,17 +2,22 @@
 // Создать строку из символов этого массива.
 
 
+string ConcatChars (char[,] chars)
+{
+    Console.Write($"Начальный массив: ");
+    string result = string.Empty;
+    for (int i = 0; i < chars.GetLength(0); i++)
+    {
+        for (int j = 0; j < chars.GetLength(1); j++)
+        {
+            Console.Write($"'{chars[i, j]}' ");
+            result += chars[i, j];
+        }
+    }
+    Console.WriteLine();
+    return result;
+}
+
 Console.Clear();
 char[,] chars = {{'H', 'e', 'l', 'l', 'o', ' '}, {'w', 'o', 'r', 'l', 'd', '!'}};
-string result = string.Empty;
-Console.Write($"Начальный массив: ");
-for (int i = 0; i < chars.GetLength(0); i++)
-{
-    for (int j = 0; j < chars.GetLength(1); j++)
-    {
-        Console.Write($"'{chars[i, j]}' ");
-        result += chars[i, j];
-    }
-}
-Console.WriteLine();
-Console.WriteLine($"Итоговая строка: {result}");
+Console.WriteLine($"Итоговая строка: {ConcatChars(chars)}");
--- Lesson06/Task02/Program.cs
+++ Lesson06/Task02/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Задайте строку, содержащую латинские буквы в обоих регистрах.
 // Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
 Console.Clear();
 string str = "Hello World";
 Console.WriteLine($"Изначальная строка: {str}");
 string strLower = str.ToLower();
+
 Console.WriteLine($"Итоговая строка: {strLower}");
